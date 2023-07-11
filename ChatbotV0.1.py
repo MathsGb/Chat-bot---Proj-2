2088a3428f40ad96f40e9e0d470a7ef761e55755
MathsGb <108474535+MathsGb@users.noreply.github.com>
2023-07-11 08:20:27 -0300

    ChatV0.2 finalizado

--- ChatbotV0.1.py
+++ ChatbotV0.1.py
@@ -29,17 +29,17 @@ def Inicio(mensagem):
 
 Sou Lenina e estou aqui para ajudar em como se comunicar com a escolaridade, no futuro.... :P
 
-Diga vc é um Aluno? /sim ou /nao
-
 Por enquanto que tal aproveitar alguma funções?
 
     - Você pode ver o seu /Horario
     - Olhar o site da /UABJ
     - Entre em contato com algum dos nossos /Emails
     - Ou podemos só jogar /Jokenpo
+    - Se tiver alguma dúvida dê uma olhada no nosso /FAQ
+
+Lembre-se sempre que se sentir perdido use /menu ou /help
     
     """
-
     bot.send_message(mensagem.chat.id, text)
 
 @bot.message_handler(commands=["Horario"])
@@ -62,7 +62,7 @@ def resposta(mensagem):
 def site(mensagem):
     bot.send_photo(mensagem.chat.id, "https://www.bj1.com.br/wp-content/uploads/2022/03/UABJ.jpeg")
     bot.send_message(mensagem.chat.id, "http://www.uabj.ufrpe.br")
-    bot.send_message(mensagem.chat.id, "Lembre-se se quiser recomeçar só clicar no /start")
+    bot.send_message(mensagem.chat.id, "Lembre-se se quiser recomeçar só clicar no /menu")
 
 @bot.message_handler(commands=["Emails"])
 def atalho(mensagem):
@@ -123,16 +123,15 @@ Aqui está a lista de comandos úteis:
 """
     bot.send_message(mensagem.chat.id, Text)
 
-
 @bot.message_handler(func = lambda msg: msg.text is not None and '/' not in msg.text)
 def conferir(msg):
     if(matrícula(int(msg.text)) == True):
-        Text = """Bem vindo Aluno
+        Text = """Bem vindo Aluno jardineiro !!!
+
 Talvez possa gostar de dar uma olhada no nosso /menu para ver tudo que esse Bot tem a oferecer.
 """
         bot.send_message(msg.chat.id, Text)
     else: bot.send_message(msg.chat.id, "Desculpe esse Bot só está disponível para alunos UABJ")
-
 
 @bot.message_handler(commands=["denovo"])
 def burro(mensagem):
@@ -140,17 +139,27 @@ def burro(mensagem):
 Não há como me vencer vamos pular logo pro fim , clique /aqui"""
     bot.reply_to(mensagem, text)
 
+@bot.message_handler(commands=["FAQ"])
+def perguntas(mensagem):
+    text = """Aqui estão as 10  perguntas mais frequentes:
+1° Como e quando posso saber a está em período de inscrição para bolsas universitárias?
+
+R = Acompanhe o período de inscrição para bolsas no site oficial do SigaA, basta ir na aba Bolsas > Solicitar aquisição de bolsa logo terá um um quadro com as devidas datas de início e fim. Para se informar sobre editais ou documentos necessários observe o grupo oficial daqui do telegram ou vá no nosso site oficial /UABJ que provavelmente estará lá.
+
+2° Como posso logar na rede wifi na UABJ e na rede da AEB?
+R = Ao tentar entrar na rede da UABJ vc notará que ela pede um nome de usuário assim como senha, este , portanto, varia de aluno à aluno, sendo respectivamente seu nome e senha de login no SigaA.
+Já para o caso da AEB estamos com alguns problemas em estabelecer a rede no momento então a wifi "UABJ" por enquanto é apenas para funcionários. Entretanto não se estresse, estamos trabalhando veemente para conseguir estabelecer a wifi lá.
+... TBC
+
+Iremos sempre atualiza-las conforme a demanda então se a sua dúvida não está aqui tenta manda diretamente para algum e-mail oficial na barra de /Emails. Por algum motivo ache isso trabalhoso ಠಿ⁠_⁠ಠ não se preocupe, estou trabalhando para que posso enviar mensagem diretamente daqui do telegram.
+
+Caso esteja perdido sempre pode voltar para o /menu. """
+    bot.send_message(mensagem.chat.id, text)
+
 @bot.message_handler(commands=["aqui"])
 def monke(mensagem):
     bot.send_message(mensagem.chat.id, "Fale com o criador : https://github.com/MathsGb ou dê o /start")
     # bot.send_sticker(mensagem.chat.id, "https://api.telegram.org/bot<token>/sendSticker?chat_id=<id>&file_id=CAADAgADOQADfyesDlKEqOOd72VKAg")
     # "https://i.kym-cdn.com/photos/images/newsfeed/001/867/654/334.jpg"
-
-# @bot.message_handler(func= verificar)
-# def Falhou(mensagem):
-#     text = """ Esse comando não existe, por favor insirá um comando válido ou dê /start denovo.
-#     Se quiser sair desse loop clique /aqui"""
-#     bot.send_photo(mensagem.chat.id, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmDxngEbCHtfceQvXnZ-mYH2iUU5b3JRd9sw&usqp=CAU")
-#     bot.send_message(mensagem.chat.id, text)
 
 bot.polling()
