cf42df3382890ed5bd458bddab661a6a48c520b2
MathsGb <108474535+MathsGb@users.noreply.github.com>
2023-08-07 09:01:44 -0300

    Webscrapping em funcionamento

--- webscrapping.py
+++ webscrapping.py
@@ -13,6 +13,8 @@ bot = telegram.Bot(token=TOKEN)
     # Send each message chunk to the Telegram channel
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     url = 'https://www.prg.unicamp.br/graduacao/pad/'
+    # Web scraping code
+    # url = 'https://www.bbc.com/news'
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -23,26 +25,25 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         headline_element = article.find('p')
         if headline_element is not None:
             headline = headline_element.text.strip()
-            # link = 'https://www.bbc.com' + article.find('a')['href']
-            # headlines.append({'headline': headline, 'link': link})
+            headlines.append({'headline': headline, })
 
     # Send messages to Telegram channel
-    message = 'Programa de bolsas: \n\n'
+    message = 'BBC News Headlines:\n\n'
     message_chunks = []
     for headline in headlines:
-        message_chunk = f'- {headline}\n'
-        # {headline["link"]}\n\n
+        # {headline["link"]}
+        message_chunk = f'- {headline["headline"]}\n'
         if len(message + message_chunk) > 4000:
             message_chunks.append(message)
-            message = 'Programa de bolsas (continuação):\n\n'
+            message = 'BBC News Headlines (continued):\n\n'
         message += message_chunk
 
     # Add the last message to the message_chunks list
     message_chunks.append(message)
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    # Send each message chunk to the Telegram channel
     for message_chunk in message_chunks:
         await update.message.reply_text(text=message_chunk)
-        # time.sleep(1)  # 3600 seconds = 1 hour
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("hello", hello))
