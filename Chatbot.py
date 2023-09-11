0991212d19a15dcee4f84474e5fb8e06ecdac8f6
MathsGb <108474535+MathsGb@users.noreply.github.com>
2023-09-11 15:56:11 -0300

    Bot completo

--- Chatbot.py
+++ Chatbot.py
@@ -1,54 +1,56 @@
 import telebot
 import requests, urllib
-from bs4 import BeautifulSoup
 from Secrets import TOKEN
 from Texts import Start_txt, email_text, Menu_text, Curso_text, help_Text, devs_text, periodos_text, Offline_text
 from utils import deleta_arquivos, cria_diretorio
-
+from unidecode import unidecode
 
 bot = telebot.TeleBot(TOKEN)
+
+Totoro = bot.get_sticker_set("Totoro")
 
 @bot.message_handler(commands=["start"])
 def Inicio(mensagem):
 	bot.set_chat_menu_button()
 	bot.send_message(mensagem.chat.id, Start_txt)
+	bot.send_sticker(mensagem.chat.id, Totoro.stickers[4].file_id)
 
 @bot.message_handler(commands=["menu"])
-def Inicio(mensagem):
-	bot.send_message(mensagem.chat.id, Menu_text)
+def Inicio(msg):
+	bot.send_message(msg.chat.id, Menu_text)
 
 @bot.message_handler(commands=["horario"])
-def Curso(mensagem):
-	bot.send_message(mensagem.chat.id, Curso_text)
+def Curso(msg):
+	bot.send_message(msg.chat.id, Curso_text)
 
 @bot.message_handler(commands=["computacao", "automacao", "quimica", "hidrica"])
-def resposta(mensagem):
+def resposta(msg):
 	try:
 		cria_diretorio('./temp')
 	except FileExistsError:
 		deleta_arquivos('./temp')
 
-	if mensagem.text == '/computacao':
+	if msg.text == '/computacao':
 		curso = 'eng_computacao'
-	elif mensagem.text == "/automacao":
+	elif msg.text == "/automacao":
 		curso = 'eng_controle_automacao'
-	elif mensagem.text == "/quimica":
+	elif msg.text == "/quimica":
 		curso = 'eng_quimica'
-	elif mensagem.text == "/hidrica":
+	elif msg.text == "/hidrica":
 		curso = 'eng_hidrica'
 	
 	with open("./temp/curso.txt", "w") as arquivo:
 		arquivo.write(curso)
 
-	bot.send_message(mensagem.chat.id, periodos_text)
+	bot.send_message(msg.chat.id, periodos_text)
 
 
 @bot.message_handler(commands=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
-def periodo(mensagem):
+def periodo(msg):
 	curso = None
 	periodo = None
 
-	for char in mensagem.text:
+	for char in msg.text:
 		try:
 			periodo = int(char)
 		except ValueError:
@@ -59,61 +61,59 @@ def periodo(mensagem):
 	
 	try:
 		arquivo = f'horario_{curso}_{periodo}_periodo.png'
-		url = 'http://127.0.0.1:5000/arquivos/' + arquivo
+		url = 'https://equipi.pythonanywhere.com//arquivos/' + arquivo
 		image = open('./temp/horario.png','wb')
 		image.write(urllib.request.urlopen(url).read())
 		image.close()
 
-		url_api = f'http://127.0.0.1:5000/bot{TOKEN}/sendPhoto?chat_id={mensagem.chat.id}'
+		url_api = f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={msg.chat.id}'
 		img = open('./temp/horario.png', 'rb')
 		requests.post(url_api, files={'photo': img})
 
-		# bot.send_message(mensagem.chat.id, url)
-		bot.send_message(mensagem.chat.id, "Aqui está seu horário, se quiser pode voltar ao /menu")
+		bot.send_message(msg.chat.id, "Aqui está seu horário, se quiser pode voltar ao /menu")
 
 	except Exception as error:
-		bot.send_message(mensagem.chat.id, "Houve um erro ao tentar baixar seu horário :( se quiser pode voltar ao /menu")
+		bot.send_sticker(msg.chat.id, Totoro.stickers[12].file_id)
+		bot.send_message(msg.chat.id, "Houve um erro ao tentar baixar seu horário se quiser pode voltar ao /menu")
 
 
 @bot.message_handler(commands=["UABJ"])
-def site(mensagem):
-	bot.send_photo(mensagem.chat.id, "https://www.bj1.com.br/wp-content/uploads/2022/03/UABJ.jpeg")
-	bot.send_message(mensagem.chat.id, "http://www.uabj.ufrpe.br")
-	bot.send_message(mensagem.chat.id, "Lembre-se se quiser recomeçar só clicar no /menu")
+def site(msg):
+	bot.send_photo(msg.chat.id, "https://www.bj1.com.br/wp-content/uploads/2022/03/UABJ.jpeg")
+	bot.send_message(msg.chat.id, "http://www.uabj.ufrpe.br")
+	bot.send_message(msg.chat.id, "Lembre-se se quiser recomeçar só clicar no /menu")
 
 
 @bot.message_handler(commands=["emails"])
-def atalho(mensagem):
-	bot.send_message(mensagem.chat.id, email_text)
-	bot.send_message(mensagem.chat.id, "Lembre-se se quiser recomeçar só clicar no /start")
+def atalho(msg):
+	bot.send_message(msg.chat.id, email_text)
+	bot.send_message(msg.chat.id, "Lembre-se se quiser recomeçar só clicar no /menu")
 
 @bot.message_handler(commands=["help"])
-def ajuda(mensagem):
-	bot.send_message(mensagem.chat.id, help_Text)
-
+def ajuda(msg):
+	bot.send_message(msg.chat.id, help_Text)
 
 @bot.message_handler(commands=["devs"])
-def dev(mensagem):
-	bot.send_message(mensagem.chat.id, devs_text)
+def dev(msg):
+	bot.send_message(msg.chat.id, devs_text)
 
 # ==== Busca no banco de dados ====
 
 @bot.message_handler(func = lambda msg: msg.text is not None and '/' not in msg.text)
 def conferir(msg):
 	try:
-		resposta = requests.request("GET", ("http://127.0.0.1:5000/" + msg.text))
+		resposta = requests.request("GET", ("https://equipi.pythonanywhere.com/" + unidecode(msg.text)))
 		bot.send_message(msg.chat.id, resposta)
 
 	except requests.exceptions.ConnectionError:
-		Set = bot.get_sticker_set("Totoro")
-		bot.send_sticker(msg.chat.id, Set.stickers[0].file_id)
+		bot.send_sticker(msg.chat.id, Totoro.stickers[12].file_id)
 		bot.send_message(msg.chat.id, Offline_text)
 
 # ==== Sugestão de perguntas ====
 
 @bot.message_handler(commands=["Sugestao"])
 def enviar_sugestao(mensagem):
-    faq_link = "http://127.0.0.1:5000/index_aluno"  # Substitua pelo endereço correto do FAQ
+    faq_link = "https://equipi.pythonanywhere.com/index_aluno"  
     bot.send_message(mensagem.chat.id, f"Para enviar uma sugestão, acesse o FAQ através deste link: {faq_link}")
     bot.send_message(mensagem.chat.id, "Lembre-se se quiser voltar, é só clicar no /menu")
 
--- Texts.py
+++ Texts.py
@@ -1,19 +1,18 @@
 Start_txt = """Sejá muito bem vindo a Lensbot
 
-Para iniciar Consulte o nosso /menu"""
+Para iniciar consulte o nosso /menu"""
 
 Menu_text = """Nosso bot Lenina vem com funções para ajudar voçe, aluno jardineiro, a resolver problemas de forma eficiente.
 
     - Consulte o seu horário de aula em /horario\n
     - Para se atentar a notícias olhe o site da faculdade em /UABJ\n
-    - Em caso de questão pessoal, procure mandar um email para a escolaridade aqui voçe entrará alguns /emails\n
+    - Em caso de questão pessoal procure mandar um email para a escolaridade, aqui voçe entrará alguns /emails\n
     - Sou equipada para responder perguntar que estão em nosso banco de dados, caso precise, basta digitar e vou procurar.\n
     - Caso tenha mais dúvidas ou esteja com algum erro consulte os nossos /devs\n
 
 Caso queira sugerir alguma pergunta não respondida vá para /Sugestao que talvez ela seja implementada e lembre-se sempre que se sentir perdido use /menu ou /help
     """
 
-    # - Se tiver alguma dúvida dê uma olhada no nosso /FAQ\n
 
 Curso_text = """Qual o seu curso na UABJ?
 
@@ -64,20 +63,6 @@ Aqui está a lista de comandos úteis:
 
 Caso ainda esta com alguma dúvida, pode escreve-lá agora mesmo que posso tentar encontrar no nosso banco de dados.
 """
-
-# FAQ_text = """Aqui estão as 10  perguntas mais frequentes:
-# 1° Como e quando posso saber a está em período de inscrição para bolsas universitárias?
-
-# R = Acompanhe o período de inscrição para bolsas no site oficial do SigaA, basta ir na aba Bolsas > Solicitar aquisição de bolsa logo terá um um quadro com as devidas datas de início e fim. Para se informar sobre editais ou documentos necessários observe o grupo oficial daqui do telegram ou vá no nosso site oficial /UABJ que provavelmente estará lá.
-
-# 2° Como posso logar na rede wifi na UABJ e na rede da AEB?
-# R = Ao tentar entrar na rede da UABJ vc notará que ela pede um nome de usuário assim como senha, este , portanto, varia de aluno à aluno, sendo respectivamente seu nome e senha de login no SigaA.
-# Já para o caso da AEB estamos com alguns problemas em estabelecer a rede no momento então a wifi "UABJ" por enquanto é apenas para funcionários. Entretanto não se estresse, estamos trabalhando veemente para conseguir estabelecer a wifi lá.
-# ... TBC
-
-# Iremos sempre atualiza-las conforme a demanda então se a sua dúvida não está aqui tenta manda diretamente para algum e-mail oficial na barra de /Emails. Por algum motivo ache isso trabalhoso ಠಿ⁠_⁠ಠ não se preocupe, estou trabalhando para que posso enviar mensagem diretamente daqui do telegram.
-
-# Caso esteja perdido sempre pode voltar para o /menu. """
 
 devs_text = """Fale com o bot manager :
 https://github.com/MathsGb
